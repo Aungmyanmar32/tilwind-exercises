0311691b7ed7b05da3418f562d873c1e585c7e9e
Aungmyanmar <aungmyanmar532@gmail.com>
2023-03-13 22:07:12 +0630

    finish color theme

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      headerColor: "#ffffff",
+      buttonColor: "#3f3cbb",
+      midnight: "#121063",
+      bgColor: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      bermuda: "#78dcca",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
